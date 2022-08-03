129345cb2c839697fde37c1edd8565a3578c526f
Thuotracy <tracyjacobs775@gmail.com>
2022-08-03 18:59:18 +0300

    declared html elements in main.js

---
+++ main.js
@@ -0,0 +1,6 @@
+// declaring html elements
+
+const imgDiv = document.querySelector('.profile-pic-div');
+const img = document.querySelector('#photo');
+const file = document.querySelector('#file');
+const uploadBtn = document.querySelector('#uploadBtn');
