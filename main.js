d4dc438945d722935e9649c16cf051a796d529dd
Thuotracy <tracyjacobs775@gmail.com>
2022-08-03 19:01:39 +0300

    added the effect if the user hovers on the profile div

--- main.js
+++ main.js
@@ -4,3 +4,10 @@ const imgDiv = document.querySelector('.profile-pic-div');
 const img = document.querySelector('#photo');
 const file = document.querySelector('#file');
 const uploadBtn = document.querySelector('#uploadBtn');
+
+
+// if user hover on profile div
+
+imgDiv.addEventListener('mouseenter', function(){
+    uploadBtn.getElementsByClassName.display = "block"
+});
